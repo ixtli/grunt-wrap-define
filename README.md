d1aecbe165c49d61c669edf6855584ea1570365b
Chris Galardi <ixplode@gmail.com>
2014-04-15 16:55:50 -0400

    initial code and readme update

--- README.md
+++ README.md
@@ -24,12 +24,10 @@ In your project's Gruntfile, add a section named `wrap_define` to the data objec
 
 ```js
 grunt.initConfig({
-  wrap_define: {
-    options: {
-      // Task-specific options go here.
-    },
-    your_target: {
-      // Target-specific file lists and/or options go here.
+  wrapDefine: {
+    options: {dependencies: [{name: 'jquery', extern: 'jQuery'}]},
+    files: {
+      'dest/default_options': ['src/testing', 'src/123'],
     },
   },
 });
@@ -37,17 +35,11 @@ grunt.initConfig({
 
 ### Options
 
-#### options.separator
-Type: `String`
-Default value: `',  '`
+#### options.dependencies
+Type: `Array`
+Default value: `[]`
 
-A string value that is used to do something with whatever.
-
-#### options.punctuation
-Type: `String`
-Default value: `'.'`
-
-A string value that is used to do something else with whatever else.
+An array of either strings or objects of the form `{'name': 'moduleName', 'extern': 'externName'}` that defines how to wrap the input code.
 
 ### Usage Examples
 
@@ -56,8 +48,8 @@ In this example, the default options are used to do something with whatever. So 
 
 ```js
 grunt.initConfig({
-  wrap_define: {
-    options: {},
+  wrapDefine: {
+    options: {dependencies: [{name: 'jquery', extern: 'jQuery'}]},
     files: {
       'dest/default_options': ['src/testing', 'src/123'],
     },
@@ -65,25 +57,10 @@ grunt.initConfig({
 });
 ```
 
-#### Custom Options
-In this example, custom options are used to do something else with whatever else. So if the `testing` file has the content `Testing` and the `123` file had the content `1 2 3`, the generated result in this case would be `Testing: 1 2 3 !!!`
-
-```js
-grunt.initConfig({
-  wrap_define: {
-    options: {
-      separator: ': ',
-      punctuation: ' !!!',
-    },
-    files: {
-      'dest/default_options': ['src/testing', 'src/123'],
-    },
-  },
-});
 ```
 
 ## Contributing
 In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
 
 ## Release History
-_(Nothing yet)_
+0.1.0 - Initial release
--- tasks/wrap_define.js
+++ tasks/wrap_define.js
@@ -8,43 +8,81 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+var description = require('../package.json').description;
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+module.exports = function(grunt)
+{
 
-  grunt.registerMultiTask('wrap_define', 'Wrap javascript in anonymous defines that require a named module mapped to a variable.', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
+	// Please see the Grunt documentation for more information regarding task
+	// creation: http://gruntjs.com/creating-tasks
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
+	grunt.registerMultiTask('wrapDefine', description, function()
+	{
+		// Merge task-specific and/or target-specific options with these defaults.
+		var dependencies = this.options.dependencies;
 
-      // Handle options.
-      src += options.punctuation;
+		if (!dependencies)
+		{
+			grunt.log.error('No dependencies.');
+			return;
+		}
 
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
+		var preamble = 'define([';
+		var interstice = '], function(';
+		var afterward = ') {\n';
+		var fileEnding = '\n});\n';
+		var quote = '\'';
+		var typeOfString = typeof 'foo';
 
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
-  });
+		var count = dependencies.length;
+		var name, extern;
+		for (var i = 0; i < count; i++)
+		{
+			if (typeof deps[i] === typeOfString)
+			{
+				name = extern = deps[i];
+			} else {
+				name = deps[i].name;
+				extern = deps[i].extern;
+			}
+
+			preamble = premable + quote + name + quote;
+			interstice = interstice + quote + extern + quote;
+
+			if (i < count - 1)
+			{
+				premable += ',';
+				interstice += ',';
+			}
+		}
+
+		var fileOpening = premable + interstice + afterward;
+
+		// Iterate over all specified file groups.
+		this.files.forEach(function(f) {
+			// Concat specified files.
+			var src = f.src.filter(function(filepath) {
+				// Warn on and remove invalid source files (if nonull was set).
+				if (!grunt.file.exists(filepath)) {
+					grunt.log.warn('Source file "' + filepath + '" not found.');
+					return false;
+				} else {
+					return true;
+				}
+			}).map(function(filepath) {
+				// Read file source.
+				return grunt.file.read(filepath);
+			}).join(grunt.util.normalizelf(options.separator));
+
+			// Handle options.
+			src = fileOpening + src + fileEnding;
+
+			// Write the destination file.
+			grunt.file.write(f.dest, src);
+
+			// Print a success message.
+			grunt.log.writeln('File "' + f.dest + '" created.');
+		});
+	});
 
 };
